05b576fdc3411947952f6644e52223f62e8f4a38
HugoSilva92 <hugo.f.o.silva@gmail.com>
2023-01-06 19:31:20 +0000

    Makefile

--- minitalk/client.c
+++ minitalk/client.c
@@ -3,18 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: huolivei <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: huolivei <huolivei <marvin@42.fr>>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 15:27:21 by huolivei          #+#    #+#             */
-/*   Updated: 2023/01/02 15:40:48 by huolivei         ###   ########.fr       */
+/*   Updated: 2023/01/06 19:26:41 by huolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+
 int	main(int ac, char **av)
 {
 	int	pid;
-
+	if (ac == 2)
+	{
+		ft_printf("Error!");
+		exit(EXIT_FAILURE);
+	}
 	pid = ft_atoi(av[1]);
+	kill(pid, SIGUSR1);
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -3,14 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: huolivei <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: huolivei <huolivei <marvin@42.fr>>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 10:27:23 by huolivei          #+#    #+#             */
-/*   Updated: 2023/01/02 15:27:46 by huolivei         ###   ########.fr       */
+/*   Updated: 2023/01/06 19:28:58 by huolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+void	hand(int sig)
+{
+	ft_printf("Recieved!%d", sig);
+}
 
 int	main(void)
 {
@@ -18,4 +23,7 @@ int	main(void)
 
 	pid = getpid();
 	ft_printf("%d\n", pid);
+	signal(SIGUSR1, hand);
+	while (1)
+		pause();
 }
